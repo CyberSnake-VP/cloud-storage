8786921aa032c2de8d66be78847dae05a46503b3
CyberSnake-VP <zigmundfreid89@yandex.ru>
2026-07-14 01:02:22 +0300

    Закончили этап создание таблицы users и тестовый запуск

--- src/main/java/org/cloudstorage/controller/HealthCheckController.java
+++ src/main/java/org/cloudstorage/controller/HealthCheckController.java
@@ -1,13 +1,25 @@
 package org.cloudstorage.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.cloudstorage.repository.UserRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class HealthCheckController {
+
+    private final UserRepository userRepository;
+
 
     @GetMapping("/health")
     public String health() {
         return "OK";
+    }
+
+    @GetMapping("/health/db")
+    public String dbHealth() {
+        long count = userRepository.count();
+        return "DB OK - Users in database:" + count;
     }
 }
