9f438f0397ac976e7a521ce516ce70db19450268
CyberSnake-VP <zigmundfreid89@yandex.ru>
2026-07-14 00:00:31 +0300

    Закончен первый этап

---
+++ src/main/java/org/cloudstorage/controller/HealthCheckController.java
@@ -0,0 +1,13 @@
+package org.cloudstorage.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+}
